eabf3e34be0b9080163eb5e04ae281fb3a9fe346
Kelvin Schmaltz Teixeira <kelvinsteixeira@gmail.com>
2021-11-09 18:46:22 -0300

    blah

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const authRoutes = require('./routes/authRoutes');
+const cors = require('cors')
 
 const app = express();
-
 app.use(express.json());
 app.use('/', authRoutes);
+app.use(cors());
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('Server is up and running...')
